4f5a57ee39924057fbd85160dbe480821dd0c652
AdilAhmed976 <adilahmedah976@gmail.com>
2023-01-21 16:46:36 +0530

    Basic App created

--- frontend/ibndex.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ibndex = () => {
-  return (
-    <div>ibndex</div>
-  )
-}
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      knpnopj
+    </div>
+  );
+}
+
+export default App;
